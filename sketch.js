f359ed92b0b48f8d8470fae88727b131de4d1bc6
sadiemcc <sadiekuzyk@gmail.com>
2025-11-20 09:34:58 -0600

    added shelves and made list appear on spacebar

--- sketch.js
+++ sketch.js
@@ -4,48 +4,70 @@ let gameState = "shopping";
 let x = 200;
 let y = 200;
 let radius = 50;
-let groceryListOptions = ['apples', 'bananas', 'oranges', 'cucumbers', 'potatoes', 'tomatoes', 'onions', 'bell peppers', 'lettuce', 'carrots', 'frozen pizza', 'ice cream', 'ground beef', 'sausages', 'fish', 'bacon', 'butter', 'cheese', 'eggs', 'milk', 'yogurt', 'cereal', 'cookies'];
+let groceryListOptions = ['apples', 'bananas', 'oranges', 'cucumbers', 'potatoes', 'tomatoes', 'onions', 'bell peppers', 'lettuce', 'carrots', 'frozen pizza', 'ice cream', 'ground beef', 'sausages', 'fish', 'bacon', 'butter', 'cheese', 'eggs', 'milk', 'yogurt', 'cereal', 'cookies', 'pasta', 'chips', 'canned soup', 'flour', 'sugar'];
 let chosenGroceryList = [];
 let choices;
+let randomChoice;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   randomGroceryList();
 }
 
-function draw() {
-  background(220);
-  movement();
+class Shelf{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = 100;
+  }
+
+  display(){
+    rect(this.x, this.y, this.width, this.height);
+  }
 }
 
+let topShelf = new Shelf(0, 0);
+
+function draw() {
+  background(220);
+  topShelf.display();
+  // shelving();
+  movement();
+}
 
 //randomizes what is on your grocery list
 function randomGroceryList(){
   for (let n = 0; n < 5; n++){
     let randomChoice = random(groceryListOptions);
     let choices = groceryListOptions.indexOf(randomChoice);
-    chosenGroceryList.push(groceryListOptions[randomChoice]);
+    chosenGroceryList.push(randomChoice);
     groceryListOptions.splice(choices, 1);
-    console.log(randomChoice);
   }
+  console.log(chosenGroceryList);
 }
 
+//function that makes the chosenlist appear on the white rectangle
 function wordsOnList(){
   fill(0);
   textAlign(CENTER);
-  textSize(50);
-  text("" + randomChoice[0], width/2, height/2);
+  textSize(45);
+  text("- "+chosenGroceryList[0], width/2, height/2-200);
+  text("- "+chosenGroceryList[1], width/2, height/2-100);
+  text("- "+chosenGroceryList[2], width/2, height/2);
+  text("- "+chosenGroceryList[3], width/2, height/2+100);
+  text("- "+chosenGroceryList[4], width/2, height/2+200);
   fill(255);
 }
 
 function movement(){
   circle(x, y, radius);
   //w
-  if (keyIsDown(87) && y > radius/2){
+  if (keyIsDown(87) && y > 100 + radius/2){
     y -= 5;
   }
   //a
-  if (keyIsDown(65) && x > radius/2){
+  if (keyIsDown(65) && x > 100 + radius/2){
     x -= 5;
   }
   //s
@@ -61,4 +83,11 @@ function movement(){
     rect(width/2, height/2, 400, 700);
     wordsOnList();
   }
+}
+
+function shelving(){
+  rectMode(CORNER);
+  rect(0, 0, width, 100);
+  rect(0, 0, 100, height);
+  rect(300, 300, width-600, 150);
 }
